3022bdcd33fb96de10530d5483c9509d899bd5e3
ArhumMalik3 <ArhumMalik95@gmail.com>
2020-12-14 16:37:35 -0600

    I added a model for Posts

---
+++ SocialMedia/Models/Posts.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Models
+{
+    public class Posts
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public double Likes { get; set; }
+
+        public string Comment { get; set; }
+
+
+
+    }
+}
