f6ef6028fb290400aee55e3b8ebfa131dbf3fe93
ArhumMalik3 <ArhumMalik95@gmail.com>
2020-12-17 18:43:16 -0600

    I added a model for Personal Information

---
+++ SocialMedia/Models/PersonalInformation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Models
+{
+    public class PersonalInformation
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime Birthday { get; set; }
+
+        public bool RelationshipStatus { get; set; }
+
+
+
+    }
+}
